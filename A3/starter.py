6786f43cd7cc35bb61791f11b4ea0655d7d00d0c
raoregon <55852350+raoregon@users.noreply.github.com>
2019-11-20 18:14:00 -0800

    Update starter.py

--- A3/starter.py
+++ A3/starter.py
@@ -4,18 +4,15 @@
 //the text file that contains a TL program in it
 create sList, an empty list of Stmt objects
 create symTable, an empty symbol table
-
 for each line of statement in tl.txt:
     parse the line to an Stmt object //possibly with satellite Expr objects
     add the Stmt object to sList
     if this line is labeled:
         add (label, current line number) mapping to symTable
-
 evaluate sList with symTable
 """
 import fileinput
 import sys
-import numbers
 
 sList = []
 symTable = {}
@@ -66,45 +63,72 @@ class Stmt :
         print("exprs:")
         print(exprs)
 
-        if keyword == 'let':
+        if self.keyword == 'let':
             if len(exprs) > 3:
                 internal = Expr(exprs[2], exprs[3], exprs[4]).eval(symTable)
-                print(internal)
                 exprs[2] = str(internal)
 
+            # This a digit, right? if not, set w/e is in exprs[0] to this new variable
             print(exprs[2].isdigit())
+
             if exprs[2].isdigit():
-                symTable[exprs[0]] = int(exprs[2])
+                symTable[exprs[0]] = float(exprs[2])
             else:
                 symTable[exprs[0]] = exprs[2]
 
         print("SymbolTable:")
         print(symTable)
-        print(self.exprs)
-
         print(exprs)
-        expr = Expr(exprs[0], exprs[1], exprs[2])
+
+        if self.keyword == 'print':
+            print(symTable[exprs[0]])
 
 
 lineCount = 0
 for line in fileinput.input(sys.argv[1:]):
     lines = line.strip()
+
     if 'let' in lines:
+        # takes the lines and turns them into a list of each string
+        # ie let x = 5 becomes ['let', 'x', '=', '5']
         stringList = lines.split()
+
+        # sets keyword to the first item in our stringList list
         keyword = stringList[0]
+
+        # creates an empty array that we will fill in depending on the length of the expression
         expression = []
 
         # check to make sure keyword is spelled correctly
         if keyword != "let":
-            raise Exception('Syntax error on line {}'.format(lineCount))
+            sys.exit('Syntax error on line {}'.format(lineCount))
 
+        # now that we know the keyword, we get rid of it
         stringList.remove(stringList[0])
+
+        # now we add the rest of the strings into expression
         for strings in stringList:
             expression.append(strings)
+
+        # our statement has been built, so now we add it to the list of statements
+        statement = Stmt(keyword, expression)
+        sList.append(statement)
+
+    elif 'print' in lines:
+        stringList = lines.split()
+        keyword = stringList[0]
+        expression = []
+
+        stringList.remove(stringList[0])
+
+        for strings in stringList:
+            expression.append(strings)
+
         statement = Stmt(keyword, expression)
         sList.append(statement)
 
     lineCount += 1
 
-for listItems in sList:
-    print listItems.perform(symTable)
+for statements in sList:
+    statements.perform(symTable)
+
