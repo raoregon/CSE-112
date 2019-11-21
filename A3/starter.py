d0a717166497cd99d329500d6ea3a0e0dbb54bce
raoregon <55852350+raoregon@users.noreply.github.com>
2019-11-20 17:33:31 -0800

    Update starter.py

--- A3/starter.py
+++ A3/starter.py
@@ -1,4 +1,18 @@
 #! /usr/bin/env python3
+"""
+//assume in the command prompt the user has entered the argument, tl.txt, the name of
+//the text file that contains a TL program in it
+create sList, an empty list of Stmt objects
+create symTable, an empty symbol table
+
+for each line of statement in tl.txt:
+    parse the line to an Stmt object //possibly with satellite Expr objects
+    add the Stmt object to sList
+    if this line is labeled:
+        add (label, current line number) mapping to symTable
+
+evaluate sList with symTable
+"""
 import fileinput
 import sys
 import numbers
